a5de8e7cf0a712fd3455cb3a1e0665116f56b759
AlexHlukhyi <AlexHlukhyi@users.noreply.github.com>
2020-06-11 17:51:45 +0300

    authentication changes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
+
 Route::get('categories', 'CategoryController@getCategories');
 
 Route::get('categories/{id}/products', 'ProductController@getProductsByCategoryId');
